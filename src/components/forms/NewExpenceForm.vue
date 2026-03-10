3cd3756755a557b49183b359da116f2cd9213683
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2026-03-10 14:03:39 -0300

    FEAT: Correções no estilo.

--- src/components/forms/NewExpenceForm.vue
+++ src/components/forms/NewExpenceForm.vue
@@ -11,53 +11,34 @@ const emit = defineEmits(['update:title', 'update:value', 'update:category'])
 </script>
 
 <template>
-  <div class="bg-white border-2 border-pink-200 rounded-xl shadow-sm p-4 m-4 max-w-xl mx-auto">
-
+  <div class="bg-white border-2 border-pink-200 rounded-xl shadow-sm p-4 m-4 lg:m-12 md:m-10">
     <h2 class="text-lg font-semibold text-pink-800 text-center mb-4">
       Nova despesa
     </h2>
 
     <div class="flex flex-col gap-3">
-
-      <input
-        :value="title"
-        @input="emit('update:title', $event.target.value)"
+      <input :value="title" @input="emit('update:title', $event.target.value)"
         class="w-full border border-pink-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-300"
-        placeholder="Descrição"
-      />
-
-      <input
-        :value="value"
-        @input="emit('update:value', $event.target.value)"
+        placeholder="Descrição" />
+      <input :value="value" @input="emit('update:value', $event.target.value)"
         class="w-full border border-pink-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-300"
-        placeholder="Valor"
-      />
-
-      <input
-        :value="category"
-        @input="emit('update:category', $event.target.value)"
+        placeholder="Valor" />
+      <input :value="category" @input="emit('update:category', $event.target.value)"
         class="w-full border border-pink-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-300"
-        placeholder="Categoria"
-      />
-
+        placeholder="Categoria" />
     </div>
 
     <div class="flex justify-center gap-6 mt-5">
-
       <button
-        class="border border-pink-400 text-pink-700 rounded-lg px-5 py-2 hover:bg-pink-50 transition"
-        @click="clearAll"
-      >
+        class="border border-pink-400 text-pink-700 rounded-lg px-5 py-2 hover:bg-pink-50 transition active:bg-pink-100 cursor-pointer"
+        @click="clearAll">
         Limpar tudo
       </button>
-
       <button
-        class="bg-pink-500 text-white rounded-lg px-6 py-2 hover:bg-pink-600 transition"
-        @click="addExpense"
-      >
+        class="bg-pink-500 text-white rounded-lg px-6 py-2 hover:bg-pink-600 transition active:bg-pink-900 cursor-pointer"
+        @click="addExpense">
         Adicionar
       </button>
-
     </div>
 
   </div>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -4,13 +4,13 @@ defineProps(['filter'])
 const emit = defineEmits(['changeFilter'])
 </script>
 <template>
-    <div class="bg-red-200 py-5 text-center lg:flex lg:justify-between lg:items-center lg:px-20">
+    <div class="bg-red-200 py-5 text-center lg:flex lg:justify-between lg:items-center lg:px-20" >
         <h1 class="text-pink-800 font-serif text-3xl py-2">Controle de Gastos</h1>
         <div class="mt-2 justify-between">
-            <button class="small-btn text-pink-800 text-xl my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 border-r-pink-800 border-r-2 lg:hover:text-pink-950" @click="emit('changeFilter','all')">Tudo</button>
-            <button class="small-btn text-pink-800 text-xl my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 border-r-pink-800 border-r-2 lg:hover:text-pink-950" @click="emit('changeFilter','food')">Comida</button>
-            <button class="small-btn text-pink-800 text-xl my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 border-r-pink-800 border-r-2 lg:hover:text-pink-950" @click="emit('changeFilter','transport')">Transporte</button>
-            <button class="small-btn text-pink-800 text-xl my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 lg:hover:text-pink-950" @click="emit('changeFilter','other')">Outros</button>
+            <button class="small-btn text-pink-600 text-lg my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 lg:hover:text-pink-800" :class="filter === 'all' ? 'text-pink-900 font-bold' : 'text-pink-600'" @click="emit('changeFilter','all')" >Tudo</button>
+            <button class="small-btn text-pink-600 text-lg my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 lg:hover:text-pink-800" :class="filter === 'food' ? 'text-pink-900 font-bold' : 'text-pink-600'" @click="emit('changeFilter','food')">Comida</button>
+            <button class="small-btn text-pink-600 text-lg my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 lg:hover:text-pink-800" :class="filter === 'transport' ? 'text-pink-900 font-bold' : 'text-pink-600'"  @click="emit('changeFilter','transport')">Transporte</button>
+            <button class="small-btn text-pink-600 text-lg my-2 px-4 lg:p-2 lg:pt-1 lg:px-5 lg:hover:text-pink-800" :class="filter === 'other' ? 'text-pink-900 font-bold' : 'text-pink-600'"  @click="emit('changeFilter','other')">Outros</button>
         </div>
     </div>
 </template>
