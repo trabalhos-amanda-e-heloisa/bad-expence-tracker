f91bc3d59dda00f6872d8835a4529050e563b57f
HeloLonghi <heloisa.longhi.ifc@gmail.com>
2026-03-11 08:18:00 -0300

    FEAT: Nome de arquivos alterados

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import AppHeader from './components/layout/AppHeader.vue';
-import NewExpenceForm from './components/forms/NewExpenceForm.vue';
+import NewExpenseForm from './components/forms/NewExpenseForm.vue';
 import { useExpenses } from './composables/useExpenses';
-import ExpenseList from './components/records/ExpenceList.vue';
+import ExpenseList from './components/records/ExpenseList.vue';
 
 const {
     expenses,
@@ -30,7 +30,7 @@ const {
             <ExpenseList :filtered="filtered" :total="total" :removeExpense="removeExpense" />
         </div>
         <div class="md:w-1/2">
-            <NewExpenceForm v-model:title="title" v-model:value="value" v-model:category="category"
+            <NewExpenseForm v-model:title="title" v-model:value="value" v-model:category="category"
                 :addExpense="addExpense" :clearAll="clearAll" :titleError="titleError" :valueError="valueError" />
         </div>
     </div>
